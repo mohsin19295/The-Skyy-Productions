f6a03a64be39efaecd474a4b74e494ff58e3a3b4
mohsin19295 <mohsin19295@gmail.com>
2022-05-28 17:04:07 +0530

    js functionality added

--- script.js
+++ script.js
@@ -7,48 +7,26 @@ const mobileMenu = () => {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 };
-
 menu.addEventListener("click", mobileMenu);
 
 
-// Show active menu when scrolling
-// const highlightMenu = () => {
-//     const elem = document.querySelector('.highlight');
-//     const homeMenu = document.querySelector('#home-page');
-//     const aboutMenu = document.querySelector('#about-page');
-//     const associatesMenu = document.querySelector('#associate-page');
-//     let scrollPos = window.scrollY;
-//     // console.log(scrollPos);
-  
-//     // adds 'highlight' class to my menu items
-//     if (window.innerWidth > 960 && scrollPos < 600) {
-//       homeMenu.classList.add('highlight');
-//       aboutMenu.classList.remove('highlight');
-//       return;
-//     } else if (window.innerWidth > 960 && scrollPos < 1400) {
-//       aboutMenu.classList.add('highlight');
-//       homeMenu.classList.remove('highlight');
-//       associatesMenu.classList.remove('highlight');
-//       return;
-//     } else if (window.innerWidth > 960 && scrollPos < 2345) {
-//       associatesMenu.classList.add('highlight');
-//       aboutMenu.classList.remove('highlight');
-//       return;
-//     }
-  
-//     if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
-//       elem.classList.remove('highlight');
-//     }
-//   };
-  
-//   window.addEventListener('scroll', highlightMenu);
-//   window.addEventListener('click', highlightMenu);
+// Adding 'Click Event' to menu items
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (event) {
+    event.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (event) {
-      event.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-      })
-  })
-})
+//  Close mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector(".is-active");
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.remove("active");
+  }
+};
+menuLinks.addEventListener("click", hideMobileMenu);
+navLogo.addEventListener("click", hideMobileMenu);
