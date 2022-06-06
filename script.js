7dfe786349c2cfe01dcd201d368d7c53f14ae9bf
mohsin19295 <mohsin19295@gmail.com>
2022-06-06 12:31:15 +0530

    course section added with css

--- script.js
+++ script.js
@@ -92,5 +92,15 @@ let timer = setInterval(function () {
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("timer").innerHTML = `${hours}h ${minutes}m ${seconds}`
+    if (hours < 10){
+      hours = "0"+hours;
+    }
+    if (minutes < 10){
+      minutes = "0"+minutes;
+    }
+    if (seconds < 10){
+      seconds = "0"+seconds;
+    }
+
+    document.getElementById("timer").innerHTML = `${hours}h : ${minutes}m : ${seconds}s`
 }, 1000);
