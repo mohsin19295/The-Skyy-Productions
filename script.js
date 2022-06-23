ef1d328781c3f9b896e4735d9aeb7bad4ce79cad
mohsin19295 <mohsin19295@gmail.com>
2022-06-23 14:27:35 +0530

    data fetching and more done

--- script.js
+++ script.js
@@ -30,99 +30,150 @@ const hideMobileMenu = () => {
 menuLinks.addEventListener("click", hideMobileMenu);
 navLogo.addEventListener("click", hideMobileMenu);
 
-// See more button
-let seeMore = document.getElementById("see_more");
-let init = 3;
-
-seeMore.onclick = () => {
-  let hiddenData = [...document.querySelectorAll(".work_section .work_wrapper .work_grid .work_card")];
-  console.log('hiddenData:', hiddenData.length)
-  for (var i = init; i < init + 3; i++) {
-    hiddenData[i].style.display = "grid";
-
-    // When last data occurs
-    if (i == hiddenData.length - 1) {
-      seeMore.style.display = "none";
-      break;
-    }
-  }
-  init += 3;
-};
-
 // For Slider
 let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
-// For smooth paading top after clicking on any section
-let navigationHeight = document.querySelector("nav").offsetHeight
-document.documentElement.style.setProperty("--scroll-padding", `${navigationHeight}px`)
-
+// For smooth pading top after clicking on any section
+let navigationHeight = document.querySelector("nav").offsetHeight;
+document.documentElement.style.setProperty(
+  "--scroll-padding",
+  `${navigationHeight}px`
+);
 
 // For single user timer
-if (localStorage.getItem("count_timer")) {
-  count_timer = localStorage.getItem("count_timer");
-} else {
-  count_timer = 86400;
+var interval = 86400000;
+
+function reset() {
+  localStorage.endTime = +new Date() + interval;
 }
 
-function returnValue(num) {
-  let hours = Math.floor(num / 3600);
-  num -= hours * 3600;
-  let minutes = Math.floor(num / 60);
-  let seconds = num - minutes * 60;
-
-  if (hours < 10) {
-      hours = "0" + hours;
-  }
-  if (minutes < 10) {
-      minutes = "0" + minutes;
-  }
-  if (seconds < 10) {
-      seconds = "0" + seconds;
-  }
-  return `${hours}h : ${minutes}m : ${seconds}s`
+if (!localStorage.endTime) {
+  reset();
 }
 
-function countDownTimer() {
-  document.getElementById("timer").innerHTML = returnValue(count_timer)
+setInterval(function () {
+  var remaining = localStorage.endTime - new Date().getTime();
 
-  if (count_timer <= 0) {
-      document.getElementById("timer").innerHTML = "Sorry Offer expired"
+  let sec = Math.floor((remaining / 1000) % 60);
+  let min = Math.floor((remaining / 1000 / 60) % 60);
+  let hour = Math.floor((remaining / 1000 / 60 / 60) % 24);
+
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
+  if (min < 10) {
+    min = "0" + min;
+  }
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+
+  if (remaining >= 0) {
+    document.querySelector("#timer").innerHTML = `${hour}: ${min}: ${sec}`;
   } else {
-      count_timer = count_timer - 1;
-      // returnValue(count_timer-1)
-      localStorage.setItem("count_timer", count_timer);
-      setTimeout("countDownTimer()", 1000);
+    document.querySelector("#timer").innerHTML = "Sorry Offer expired";
   }
-}
-setTimeout("countDownTimer()", 1000);
+}, 100);
 
 // Refering the course-page
 let coursePageBtn = document.getElementById("check-course-btn");
-coursePageBtn.addEventListener("click", function(){
-  window.location.href = "./course/course.html"
-})
+coursePageBtn.addEventListener("click", function () {
+  window.location.href = "./course/course.html";
+});
+
+// For YouTube Data
+// let key = "AIzaSyATSP2ynUU02GMJu-JkNL49w6bU9UD9_F8";
+var key = config.API_KEY;
+var id = config.CHANNEL_ID;
+
+let video = document.querySelector(".work_card");
+let yt_box = document.querySelector(".yt_box");
+let wrapper = document.querySelector(".work_wrapper");
+
+let head = document.querySelector(".head");
+let loadmoreDiv = document.createElement("button");
+loadmoreDiv.className = "see_more";
+loadmoreDiv.innerHTML = "See More";
+
+const myFun = () => {
+  let url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${id}&maxResults=10&order=viewCount&key=${key}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      res.items.map((e, i) => {
+        let single = document.createElement("div");
+        single.className = "youtube_video";
+        let style = document.createElement("div");
+        style.className = "youtube_style";
+
+        let logo = document.createElement("img");
+        logo.src = "https://via.placeholder.com/50";
+        let p = document.createElement("p");
+        p.innerHTML = e.snippet?.title;
+        style.append(logo, p);
+
+        let iframeDiv = document.createElement("div");
+        iframeDiv.className = "iframeDiv";
+        let iframe = document.createElement("iframe");
+        iframe.frameBorder = 0;
+        iframe.border = 0;
+        iframe.cellspacing = 0;
+        iframe.src = `https://www.youtube.com/embed/${e.id?.videoId}`;
+        iframeDiv.append(iframe);
+
+        if (i > 2) {
+          single.classList.add("hidden");
+        }
+
+        single.append(iframeDiv, style);
+        video.append(single);
+        yt_box.append(video, loadmoreDiv);
+        wrapper.append(yt_box);
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+let init = 3;
+loadmoreDiv.onclick = () => {
+  let total = [...document.querySelectorAll(".work_card .youtube_video")];
+
+  for (var i = init; i < init + 3; i++) {
+    total[i].style.display = "inline-block";
+    if (i == total.length - 1) {
+      loadmoreDiv.style.display = "none";
+      break;
+    }
+  }
+  init += 3;
+};
+myFun();
